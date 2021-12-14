0331f7c60ca518373f3cbd1bef3eb25b3172a0b3
Jamie Kirkwin <jkirkwin@gmail.com>
2021-12-14 01:35:09 -0800

    Add error to bulk simulation runner

--- run_simulations.py
+++ run_simulations.py
@@ -11,18 +11,29 @@ import shutil
 import sys
 from pathlib import Path
 
-bitrates = ["100Kbps", "500Kbps", "1Mbps", "2Mbps", "3Mbps", "4Mbps","5Mbps", "10Mbps", "100Mbps", "500Mbps", "1Gbps"]
-algos = ["panda", "festive", "tobasco"]
-pacingVals = ["true", "false"]
+bitrates = ["500Kbps", "1Mbps", "2Mbps", "3Mbps", "5Mbps"]
+algos = ["festive", "tobasco"]
+pacingVals = [True, False]
 protocols = ["QUIC", "TCP"]
+errs = [0, 0.01, 0.02, 0.05]
 
-def getWafCommand(protocol, bitrate, algo, pacing, simId):
+class SimulationConfig:
+    def __init__ (self, simId, protocol, algo, bitrate, err, pacing):
+        self.simId = simId
+        self.protocol = protocol
+        self.algo = algo
+        self.bitrate = bitrate
+        self.err = err
+        self.pacing = pacing       
+
+def getWafCommand(config):
     args = [
-        '--simulationId={}'.format(simId), 
-        '--adaptationAlgo={}'.format(algo), 
-        '--dataRate={}'.format(bitrate), 
-        '--pacingEnabled={}'.format(pacing), 
-        '--transportProtocol={}'.format(protocol)
+        '--simulationId={}'.format(config.simId), 
+        '--adaptationAlgo={}'.format(config.algo), 
+        '--dataRate={}'.format(config.bitrate), 
+        '--pacingEnabled={}'.format(config.pacing), 
+        '--transportProtocol={}'.format(config.protocol),
+        '--errorRate={}'.format(config.err)
     ]
 
     argsStr = " ".join(args)
@@ -30,15 +41,18 @@ def getWafCommand(protocol, bitrate, algo, pacing, simId):
     return './waf --run="dash-streaming {}"'.format(argsStr)
 
 def getPacingString(pacing):
-    return "pacing" if pacing == "true" else "no-pacing"
+    if pacing:
+        return "pacing"
+    else:
+        return "no-pacing"
 
-def getOutputFileName(protocol, bitrate, algo, pacing):
-    pacingStr = getPacingString(pacing)
-    return "output_{}_{}_{}_{}.log".format(protocol, algo, bitrate, pacingStr)
+def getOutputFileName(config):
+    pacingStr = getPacingString(config.pacing)
+    return "output_{}_{}_{}_{}.log".format(config.protocol, config.algo, config.bitrate, pacingStr)
 
-def getOutputDir(baseDir, protocol, bitrate, algo, pacing):
-    pacingStr = getPacingString(pacing)
-    return '{}/{}/{}/{}/{}/'.format(baseDir, protocol, algo, bitrate, pacingStr) # QUIC/festive/1Mbps/pacing for example
+def getOutputDir(baseDir, config):
+    pacingStr = getPacingString(config.pacing)
+    return '{}/{}/{}/{}/{}/{}/'.format(baseDir, config.protocol, config.algo, config.bitrate, config.err, pacingStr) # QUIC/festive/1Mbps/0.01/pacing for example
 
 def copyFiles(srcDir, destDir):
     srcFiles = os.listdir(srcDir)
@@ -47,48 +61,67 @@ def copyFiles(srcDir, destDir):
         if os.path.isfile(full_file_name):
             shutil.copy(full_file_name, destDir)
 
-def getConfigString(protocol, bitrate, algo, pacing, simId):
-    pacingStr = "pacing" if pacing == "true" else "no pacing"
-    return 'Simulation {}: {} over {} @ {} - {}'.format(simId, algo, protocol, bitrate, pacingStr)
+def getConfigString(config):
+    pacingStr = getPacingString(config.pacing)
+    errStr = str(config.err*100)
+    return 'Simulation {}: {} over {} @ {} with {} percent error - {}'.format(config.simId, config.algo, config.protocol, config.bitrate, errStr, pacingStr)
 
-def runSimulation(baseDir, protocol, bitrate, algo, pacing, simId):
-    print (getConfigString(protocol, bitrate, algo, pacing, simId))
+def countSuccessfulDownloads(logDir):
+    downloadLogFile = logDir + "cl0_downloadLog.txt"
+
+    lastline = ''
+    with open(downloadLogFile) as f:
+        lastline = f.readlines()[-1]
+
+    maxSegment = int(lastline.split()[0]) 
+    return maxSegment + 1
+
+def runSimulation(baseDir, config):
+    print (getConfigString(config))
 
     # Ensure that the output directory is created   
-    outputDir = getOutputDir(baseDir, protocol, bitrate, algo, pacing)
+    outputDir = getOutputDir(baseDir, config)
     Path(outputDir).mkdir(parents=True, exist_ok=True)
+    print ('Created output dir: ', outputDir)
 
     # Run the simulation
     print ("Running...")
-    outputPath = outputDir + getOutputFileName(protocol, bitrate, algo, pacing)
-    command = getWafCommand(protocol, bitrate, algo, pacing, simId)
+    outputPath = outputDir + getOutputFileName(config)
+    command = getWafCommand(config)
     with open(outputPath, "w") as outputFile:
         subprocess.run(command, shell=True, stdout=outputFile, stderr=subprocess.STDOUT)
     
     # Copy the logs into the output directory
-    logFilesDir = "dash-log-files/{}/{}/".format(algo, simId)
+    logFilesDir = "dash-log-files/{}/{}/".format(config.algo, config.simId)
     copyFiles(logFilesDir, outputDir)
+
+    # Count the number of segments that were successfully downloaded
+    count = countSuccessfulDownloads(logFilesDir)
+    print ("{} segments downloaded successfully".format(count))
 
     print ("Done simulation\n")
 
-def generateConfigs():
-    id = 0
+
+def generateConfigs(startingSimId = 0):
+    id = startingSimId
     for proto in protocols:
         for algo in algos:
             for bitrate in bitrates:
-                for pacingVal in pacingVals:
-                    yield [proto, bitrate, algo, pacingVal, id]
-                    id = id + 1
+                for err in errs:
+                    for pacingVal in pacingVals:
+                        yield SimulationConfig(id, proto, algo, bitrate, err, pacingVal)
+                        id = id + 1
 
-def main(baseDir):
-    for (protocol, rate, algo, pacing, id) in generateConfigs():
-        runSimulation(baseDir, protocol, rate, algo, pacing, id)
+def main(baseDir, startingSimId):
+    for config in generateConfigs(startingSimId):
+        runSimulation(baseDir, config)
 
 if (__name__ == '__main__'):
-    if len(sys.argv) != 2:
-        print ("ERROR: Invalid number of arguments. Provide a single argument for the output directory.")
+    if len(sys.argv) != 3:
+        print ("Usage: <output dir> <starting simulation id>")
         exit(1)
     
     baseDir = sys.argv[1]
+    startingId = int(sys.argv[2])
     print ("Saving all log files under {}\n".format(baseDir))
-    main(baseDir)
+    main(baseDir, startingId)
